c16781a8aeb7e70c491a87b4ca6cdb9c88160714
eddie ciccone <ecicc2@uis.edu>
2022-05-24 18:28:51 -0500

    created tests for SelectRecipeById

--- api/repo/recipe/recipe_repo_test.go
+++ api/repo/recipe/recipe_repo_test.go
@@ -8,6 +8,103 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_SelectRecipeById(t *testing.T) {
+	data := []struct {
+		Name        string
+		R           Recipe
+		ExpectedSQL func(sqlmock.Sqlmock, Recipe)
+		Pass        bool
+		Assert      func(sqlmock.Sqlmock, Recipe, Recipe, error)
+	}{
+		{
+			Name: "select recipe",
+			R: Recipe{
+				Id:        1,
+				Name:      "Test Recipe",
+				Username:  "Test User",
+				ImageName: "test-img.png",
+				Ingredients: []Ingredient{
+					{Id: 1, Name: "Ingredient 1", Amount: "1", Unit: "tbsp", RecipeId: 1},
+					{Id: 2, Name: "Ingredient 2", Amount: "1", Unit: "cups", RecipeId: 1},
+				},
+			},
+			ExpectedSQL: func(mock sqlmock.Sqlmock, recipe Recipe) {
+				recipeRow := sqlmock.NewRows([]string{"id", "name", "username", "imagename"}).
+					AddRow(recipe.Id, recipe.Name, recipe.Username, recipe.ImageName)
+				mock.ExpectQuery("SELECT id, name, username, imagename FROM recipe WHERE id = ?").
+					WithArgs(recipe.Id).WillReturnRows(recipeRow)
+
+				ingredientRows := sqlmock.NewRows([]string{"id", "name", "amount", "unit", "recipeid"})
+				for _, i := range recipe.Ingredients {
+					ingredientRows.AddRow(i.Id, i.Name, i.Amount, i.Unit, i.RecipeId)
+				}
+				mock.ExpectQuery("SELECT id, name, amount, unit, recipeid FROM ingredient WHERE recipeid = ?").
+					WithArgs(recipe.Id).
+					WillReturnRows(ingredientRows)
+			},
+			Pass: true,
+			Assert: func(mock sqlmock.Sqlmock, expected, result Recipe, err error) {
+				assert.NoError(t, err)
+				assert.Equal(t, expected, result)
+			},
+		},
+		{
+			Name: "select recipe error",
+			R: Recipe{
+				Id:          1,
+				Ingredients: []Ingredient{},
+			},
+			ExpectedSQL: func(mock sqlmock.Sqlmock, recipe Recipe) {
+				mock.ExpectQuery("SELECT id, name, username, imagename FROM recipe WHERE id = ?").
+					WithArgs(recipe.Id).WillReturnError(errors.New("error selecting recipe"))
+			},
+			Pass: false,
+			Assert: func(mock sqlmock.Sqlmock, expected, result Recipe, err error) {
+				assert.Error(t, err)
+				assert.Equal(t, Recipe{}, result)
+			},
+		},
+		{
+			Name: "select recipe ingredient error",
+			R: Recipe{
+				Id:        1,
+				Name:      "Test Recipe",
+				Username:  "Test User",
+				ImageName: "test-img.png",
+				Ingredients: []Ingredient{
+					{Id: 1, Name: "Ingredient 1", Amount: "1", Unit: "tbsp", RecipeId: 1},
+					{Id: 2, Name: "Ingredient 2", Amount: "1", Unit: "cups", RecipeId: 1},
+				},
+			},
+			ExpectedSQL: func(mock sqlmock.Sqlmock, recipe Recipe) {
+				recipeRow := sqlmock.NewRows([]string{"id", "name", "username", "imagename"}).
+					AddRow(recipe.Id, recipe.Name, recipe.Username, recipe.ImageName)
+				mock.ExpectQuery("SELECT id, name, username, imagename FROM recipe WHERE id = ?").
+					WithArgs(recipe.Id).WillReturnRows(recipeRow)
+
+				mock.ExpectQuery("SELECT id, name, amount, unit, recipeid FROM ingredient WHERE recipeid = ?").
+					WithArgs(recipe.Id).WillReturnError(errors.New("error selecting ingredients"))
+			},
+			Pass: false,
+			Assert: func(mock sqlmock.Sqlmock, expected, result Recipe, err error) {
+				assert.Error(t, err)
+				assert.Equal(t, Recipe{}, result)
+			},
+		},
+	}
+
+	db, mock, _ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+
+	for _, d := range data {
+		t.Log("TEST: ", d.Name)
+
+		d.ExpectedSQL(mock, d.R)
+		rr := NewRepo(db)
+		result, err := rr.SelectRecipeById(d.R.Id)
+		d.Assert(mock, d.R, result, err)
+	}
+}
+
 func Test_InsertRecipe(t *testing.T) {
 	data := []struct {
 		Name        string
@@ -125,8 +222,8 @@ func Test_InsertRecipe(t *testing.T) {
 
 	for _, d := range data {
 		t.Log("TEST: ", d.Name)
-		mock.ExpectBegin()
 
+		mock.ExpectBegin()
 		d.ExpectedSQL(mock, d.R)
 
 		if d.Pass {
