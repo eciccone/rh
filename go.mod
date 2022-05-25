1f1532a1145666eec4838caf568cc2ceb060434e
eddie ciccone <ecicc2@uis.edu>
2022-05-25 15:57:50 -0500

    updated main to broadcast endpoints

--- go.mod
+++ go.mod
@@ -5,12 +5,15 @@ go 1.16
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/lestrrat-go/jwx v1.2.25 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-sqlite3 v1.14.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/ugorji/go v1.2.7 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,118 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/eciccone/rh/api/handler"
+	"github.com/eciccone/rh/api/middleware"
+	"github.com/eciccone/rh/api/repo/profile"
+	"github.com/eciccone/rh/api/service"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	dbfile = "recihub.db"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if godotenv.Load() != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connName := fmt.Sprintf("%v?_foreign_keys=on", dbfile)
+	db, err := sql.Open("sqlite3", connName)
+	if err != nil {
+		log.Fatal("failed to connect to database: " + dbfile)
+	}
+	defer db.Close()
+	CreateSQLiteTables(db)
+
+	pr := profile.NewRepo(db)
+	// rr := recipe.NewRepo(db)
+
+	ps := service.NewProfileService(pr)
+
+	ph := handler.NewProfileHandler(ps)
+
+	router := setupRouter()
+	router.Use(middleware.Validate())
+
+	router.GET("/profile", ph.GetProfile)
+	router.POST("/profile", ph.PostProfile)
+
+	router.Run(":8080")
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.New()
+
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowHeaders:     []string{"Access-Control-Allow-Origin", "*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+
+	r.Use(cors.New(config))
+
+	return r
+}
+
+const createProfileTable = `
+	CREATE TABLE IF NOT EXISTS profile (
+  	id TEXT NOT NULL PRIMARY KEY,
+  	username TEXT NOT NULL UNIQUE
+  );`
+
+const createRecipeTable = `
+	CREATE TABLE IF NOT EXISTS recipe (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		username TEXT NOT NULL,
+		imagename TEXT default "",
+		CHECK (name <> '' AND username <> '')
+	);`
+
+const createIngredientTable = `
+	CREATE TABLE IF NOT EXISTS ingredient (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		amount TEXT NOT NULL,
+		unit TEXT NOT NULL,
+		recipeid INTEGER NOT NULL,
+		FOREIGN KEY(recipeid) REFERENCES recipe(id) ON DELETE CASCADE
+	);`
+
+const createStepTable = `
+	CREATE TABLE IF NOT EXISTS step (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		stepnumber INTEGER NOT NULL,
+		description TEXT NOT NULL,
+		recipeid INTEGER NOT NULL,
+		FOREIGN KEY(recipeid) REFERENCES recipe(id) ON DELETE CASCADE
+	);`
+
+func CreateSQLiteTables(conn *sql.DB) {
+	if _, err := conn.Exec(createProfileTable); err != nil {
+		log.Fatal("failed to create PROFILE table")
+	}
+
+	if _, err := conn.Exec(createRecipeTable); err != nil {
+		log.Fatal("failed to create RECIPE table")
+	}
+
+	if _, err := conn.Exec(createIngredientTable); err != nil {
+		log.Fatal("failed to create INGREDIENT table")
+	}
+
+	if _, err := conn.Exec(createStepTable); err != nil {
+		log.Fatal("failed to create STEP table")
+	}
 }
