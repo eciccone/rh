c1a2106251d9a29f71a949b11253b60c0131cf51
eddie ciccone <ecicc2@uis.edu>
2022-05-25 00:28:13 -0500

    updated recipe_service_test to test more effectively

--- api/service/recipe_service_test.go
+++ api/service/recipe_service_test.go
@@ -9,166 +9,145 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type mockRecipeRepo struct{}
+type RecipeRepoMocker struct {
+	InsertRecipeMock            func(recipe recipe.Recipe) (recipe.Recipe, error)
+	SelectRecipeByIdMock        func(id int) (recipe.Recipe, error)
+	SelectRecipesByUsernameMock func(username string, orderBy string, offset int, limit int) ([]recipe.Recipe, error)
+	UpdateRecipeMock            func(recipe recipe.Recipe) (recipe.Recipe, error)
+	UpdateRecipeImageNameMock   func(id int, imageName string) error
+	DeleteRecipeMock            func(id int) error
+}
 
 func NewMockRecipeRepo() recipe.RecipeRepository {
-	return &mockRecipeRepo{}
+	return &RecipeRepoMocker{}
 }
 
-func (r *mockRecipeRepo) InsertRecipe(args recipe.Recipe) (recipe.Recipe, error) {
-	if args.Id == -1 {
-		return recipe.Recipe{}, errors.New("failed to insert recipe")
-	}
-
-	return recipe.Recipe{
-		Id:       1,
-		Name:     args.Name,
-		Username: args.Username,
-	}, nil
+func (r *RecipeRepoMocker) InsertRecipe(args recipe.Recipe) (recipe.Recipe, error) {
+	return r.InsertRecipeMock(args)
 }
 
-func (r *mockRecipeRepo) SelectRecipeById(id int) (recipe.Recipe, error) {
-	if id == 99 {
-		return recipe.Recipe{}, sql.ErrNoRows
-	}
-
-	if id == 100 {
-		return recipe.Recipe{}, errors.New("failed to select recipe")
-	}
-
-	return recipe.Recipe{Id: id}, nil
+func (r *RecipeRepoMocker) SelectRecipeById(id int) (recipe.Recipe, error) {
+	return r.SelectRecipeByIdMock(id)
 }
 
-func (r *mockRecipeRepo) SelectRecipesByUsername(username string, orderBy string, offset int, limit int) ([]recipe.Recipe, error) {
-	var result []recipe.Recipe
-
-	return result, nil
+func (r *RecipeRepoMocker) SelectRecipesByUsername(username string, orderBy string, offset int, limit int) ([]recipe.Recipe, error) {
+	return r.SelectRecipesByUsernameMock(username, orderBy, offset, limit)
 }
 
-func (r *mockRecipeRepo) UpdateRecipe(args recipe.Recipe) (recipe.Recipe, error) {
-	var result recipe.Recipe
-
-	return result, nil
+func (r *RecipeRepoMocker) UpdateRecipe(args recipe.Recipe) (recipe.Recipe, error) {
+	return r.UpdateRecipeMock(args)
 }
 
-func (r *mockRecipeRepo) UpdateRecipeImageName(id int, imageName string) error {
-	return nil
+func (r *RecipeRepoMocker) UpdateRecipeImageName(id int, imageName string) error {
+	return r.UpdateRecipeImageNameMock(id, imageName)
 }
 
-func (r *mockRecipeRepo) DeleteRecipe(id int) error {
-	return nil
-}
-
-func Test_GetRecipe(t *testing.T) {
-	data := []struct {
-		Id     int
-		Assert func(int, recipe.Recipe, error)
-	}{
-		{
-			Id: 1,
-			Assert: func(i int, r recipe.Recipe, e error) {
-				assert.NoError(t, e)
-				assert.Equal(t, i, r.Id)
-			},
-		},
-		{
-			Id: 0,
-			Assert: func(i int, r recipe.Recipe, e error) {
-				assert.Error(t, e)
-				assert.Zero(t, r.Id)
-			},
-		},
-		{
-			Id: 99,
-			Assert: func(i int, r recipe.Recipe, e error) {
-				assert.Error(t, e)
-				assert.Zero(t, r.Id)
-			},
-		},
-		{
-			Id: 100,
-			Assert: func(i int, r recipe.Recipe, e error) {
-				assert.Error(t, e)
-				assert.Zero(t, r.Id)
-			},
-		},
-	}
-
-	for _, d := range data {
-		rs := NewRecipeService(NewMockRecipeRepo())
-		r, err := rs.GetRecipe(d.Id)
-		d.Assert(d.Id, r, err)
-	}
+func (r *RecipeRepoMocker) DeleteRecipe(id int) error {
+	return r.DeleteRecipeMock(id)
 }
 
 func Test_CreateRecipe(t *testing.T) {
-	data := []struct {
-		Id          int
-		Name        string
-		Username    string
-		Ingredients []recipe.Ingredient
-		Test        func(string, string, []recipe.Ingredient)
-		Assert      func(recipe.Recipe, recipe.Recipe, error)
+	td := []struct {
+		Input    recipe.Recipe
+		Expected recipe.Recipe
+		InsertFn func(args recipe.Recipe) (recipe.Recipe, error)
+		Assert   func(expected recipe.Recipe, actual recipe.Recipe, err error)
 	}{
 		{
-			Name:     "Test Name",
-			Username: "Test User",
-			Ingredients: []recipe.Ingredient{
-				{Name: "ingredient 1", Amount: "2", Unit: "tbsp"},
+			Input:    recipe.Recipe{Name: "Test Name", Username: "Test User"},
+			Expected: recipe.Recipe{Id: 1, Name: "Test Name", Username: "Test User"},
+			InsertFn: func(args recipe.Recipe) (recipe.Recipe, error) {
+				args.Id = 1
+				return args, nil
 			},
-			Assert: func(r1, r2 recipe.Recipe, err error) {
+			Assert: func(expected, actual recipe.Recipe, err error) {
 				assert.NoError(t, err)
-				assert.NotZero(t, r2.Id)
-				assert.Equal(t, r1.Name, r2.Name)
-				assert.Equal(t, r1.Username, r2.Username)
+				assert.Equal(t, expected, actual)
 			},
 		},
 		{
-			Name:     "",
-			Username: "Test User",
-			Ingredients: []recipe.Ingredient{
-				{Name: "ingredient 1", Amount: "2", Unit: "tbsp"},
+			Input:    recipe.Recipe{Name: "Test Name", Username: "Test User"},
+			Expected: recipe.Recipe{},
+			InsertFn: func(args recipe.Recipe) (recipe.Recipe, error) {
+				return recipe.Recipe{}, errors.New("failed")
 			},
-			Assert: func(r1, r2 recipe.Recipe, err error) {
+			Assert: func(expected, actual recipe.Recipe, err error) {
 				assert.Error(t, err)
-				assert.Empty(t, r2)
+				assert.Equal(t, expected, actual)
 			},
 		},
 		{
-			Name:     "Test Name",
-			Username: "",
-			Ingredients: []recipe.Ingredient{
-				{Name: "ingredient 1", Amount: "2", Unit: "tbsp"},
-			},
-			Assert: func(r1, r2 recipe.Recipe, err error) {
+			Input:    recipe.Recipe{},
+			Expected: recipe.Recipe{},
+			Assert: func(expected, actual recipe.Recipe, err error) {
 				assert.Error(t, err)
-				assert.Empty(t, r2)
-			},
-		},
-		{
-			Id:       -1,
-			Name:     "Test Name",
-			Username: "Test User",
-			Ingredients: []recipe.Ingredient{
-				{Name: "ingredient 1", Amount: "2", Unit: "tbsp"},
-			},
-			Assert: func(r1, r2 recipe.Recipe, err error) {
-				assert.Error(t, err)
-				assert.Empty(t, r2)
+				assert.Equal(t, expected, actual)
 			},
 		},
 	}
 
-	for _, d := range data {
-		rs := NewRecipeService(NewMockRecipeRepo())
-		r1 := recipe.Recipe{
-			Id:          d.Id,
-			Name:        d.Name,
-			Username:    d.Username,
-			Ingredients: d.Ingredients,
-		}
+	for _, tr := range td {
+		rr := &RecipeRepoMocker{InsertRecipeMock: tr.InsertFn}
+		rs := NewRecipeService(rr)
+		result, err := rs.CreateRecipe(tr.Input)
+		tr.Assert(tr.Expected, result, err)
+	}
+}
 
-		r2, err := rs.CreateRecipe(r1)
-		d.Assert(r1, r2, err)
+func Test_GetRecipe(t *testing.T) {
+	td := []struct {
+		Input    int
+		Expected recipe.Recipe
+		SelectFn func(id int) (recipe.Recipe, error)
+		Assert   func(expected recipe.Recipe, actual recipe.Recipe, err error)
+	}{
+		{
+			Input:    1,
+			Expected: recipe.Recipe{Id: 1, Name: "Test Name", Username: "Test User"},
+			SelectFn: func(id int) (recipe.Recipe, error) {
+				return recipe.Recipe{Id: 1, Name: "Test Name", Username: "Test User"}, nil
+			},
+			Assert: func(expected, actual recipe.Recipe, err error) {
+				assert.NoError(t, err)
+				assert.Equal(t, expected, actual)
+			},
+		},
+		{
+			Input:    1,
+			Expected: recipe.Recipe{},
+			SelectFn: func(id int) (recipe.Recipe, error) {
+				return recipe.Recipe{}, errors.New("fail")
+			},
+			Assert: func(expected, actual recipe.Recipe, err error) {
+				assert.Error(t, err)
+				assert.Equal(t, expected, actual)
+			},
+		},
+		{
+			Input:    1,
+			Expected: recipe.Recipe{},
+			SelectFn: func(id int) (recipe.Recipe, error) {
+				return recipe.Recipe{}, sql.ErrNoRows
+			},
+			Assert: func(expected, actual recipe.Recipe, err error) {
+				assert.Error(t, err)
+				assert.Equal(t, expected, actual)
+			},
+		},
+		{
+			Input:    0,
+			Expected: recipe.Recipe{},
+			Assert: func(expected, actual recipe.Recipe, err error) {
+				assert.Error(t, err)
+				assert.Equal(t, expected, actual)
+			},
+		},
+	}
+
+	for _, tr := range td {
+		rr := &RecipeRepoMocker{SelectRecipeByIdMock: tr.SelectFn}
+		rs := NewRecipeService(rr)
+		result, err := rs.GetRecipe(tr.Input)
+		tr.Assert(tr.Expected, result, err)
 	}
 }
